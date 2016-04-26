5525652dd7c920495b6aafdf005fb772ccafacec
YiwenTheSleepy <yiwen.efrei@gmail.com>
2016-04-26 23:54:02 +0200

    add team info

--- src/app/pages/team/team.controller.js
+++ src/app/pages/team/team.controller.js
@@ -8,5 +8,13 @@
   /** @ngInject */
   function TeamController(team) {
     console.log(team);
+	var vm = this;
+	vm.ini = function() {
+		
+		vm.name = team.data.name;
+		vm.sName = team.data.shortName;
+		vm.marketValue = team.data.squadMarketValue;
+		vm.url = team.data.crestUrl;
+	}
   }
 })();
